52bd1a2f5f9b52642e1e2cf9baee7561c91a6bde
hasPatrickC <patrickc@hasoffers.com>
2018-03-28 13:39:01 -0700

    switch parent container to closest string

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react';
+import * as React from 'react';
+import { Link, Route } from 'react-router-dom';
 import './App.css';
+
+
 
 import Container from './reddit/353-closest-string/Container';
 import Highlighter from './other/phrase-highlighter/App';
 
-class App extends Component {
+class App extends React.Component {
   render() {
+
+    return <Container />;
     return (
-      <Highlighter />
+      <div>
+        <div>
+          <Link to="/353-closest-string">Closest String</Link>
+          <Link to="/phrase-highlighter">Phrase Highlighter</Link>
+        </div>
+        <div>
+          <Route path="/353-closest-string" component={Container} />
+          <Route path="/phrase-highlighter" component={Highlighter} />
+        </div>
+      </div>
     );
   }
 }
